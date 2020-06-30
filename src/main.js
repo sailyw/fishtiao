72dfe994fcebd46e3ac83f5329f5628a91a271c4
sailyw <1152827086@qq.com>
2020-06-30 15:46:07 +0800

    初始化基础配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
