546506f19d47d64240e59114d5be4a8455f73825
sailyw <1152827086@qq.com>
2020-07-02 13:17:36 +0800

    修改验证码倒计时不显示问题以及增加我的页面和退出登录

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage'
 
 Vue.use(Vuex)
+// 这样做的目的可以避免访问和获取数据的名字不一致导致的问题
+const USER_KEY = 'toutiao-user'
 
 export default new Vuex.Store({
   state: {
+    user: getItem(USER_KEY), // 当前登录用户的登录状态（token等数据）
+    // user: JSON.parse(window.localStorage.getItem('user')) // 当前登录用户的登录状态（token等数据）
+    cachePages: ['LayoutIndex']
   },
   mutations: {
+    setUser (state, data) {
+      state.user = data
+
+      // 为了防止页面刷新数据丢失，我们还需要把数据放到本地存储中，这里仅仅是为了持久化数据
+      setItem(USER_KEY, state.user)
+      // window.localStorage.setItem('user', JSON.stringify(state.user))
+    },
+
+    // 添加缓存页面
+    addCachePage (state, pageName) {
+      if (!state.cachePages.includes(pageName)) {
+        state.cachePages.push(pageName)
+      }
+    },
+
+    // 移除缓存页面
+    removeCachePage (state, pageName) {
+      const index = state.cachePages.indexOf(pageName)
+      if (index !== -1) {
+        state.cachePages.splice(index, 1)
+      }
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
