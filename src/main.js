f5bc246f9c9a791f6ed4381ca4a6b4385d70d92a
sailyw <1152827086@qq.com>
2020-07-02 15:04:12 +0800

    添加首页的文章列表内容

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import './utils/dayjs'
 Vue.use(Vant)
 
 Vue.config.productionTip = false
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,98 @@
 <template>
-  <div class="home-container">home</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="app-nav-bar">
+      <van-button
+        slot="title"
+        class="search-btn"
+        icon="search"
+        type="info"
+        round
+        size="small"
+      >搜索</van-button>
+    </van-nav-bar>
+    <!-- /导航栏 -->
+
+    <!-- 文章频道列表 -->
+    <!-- <van-tabs v-model="active">
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs> -->
+    <van-tabs v-model="active">
+      <van-tab
+        :title="channel.name"
+        v-for="channel in channels"
+        :key="channel.id"
+      >
+        <!-- 文章列表 -->
+        <article-list :channel="channel"/>
+        <!-- /文章列表 -->
+      </van-tab>
+    </van-tabs>
+    <!-- /文章频道列表 -->
+  </div>
 </template>
 
 <script>
+import { getUserChannels } from '@/api/user'
+import ArticleList from './components/article-list'
 export default {
   name: 'HomeIndex',
-  components: {},
+  components: {
+    ArticleList
+  },
   props: {},
   data () {
-    return {}
+    return {
+      active: 0, // 控制被激活的标签
+      channels: []// 频道列表
+    }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadChannels()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    async loadChannels () {
+      // 请求获取频道数据
+      const { data } = await getUserChannels()
+      this.channels = data.data.channels
+    }
+  }
 }
 </script>
-
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: none;
+  }
+  .search-btn {
+    width: 277px;
+    height: 32px;
+    background: #5babfb;
+    border: none;
+    .van-icon {
+      font-size: 16px;
+    }
+    .van-button__text {
+      font-size: 14px;
+    }
+  }
+  .channel-tabs {
+    /deep/ .van-tab {
+      border-right: 1px solid #edeff3;
+      border-bottom: 1px solid #edeff3;
+    }
+    /deep/ .van-tabs__line {
+      bottom: 20px;
+      width: 15px !important;
+      height: 3px;
+      background: #3296fa;
+    }
+  }
+}
+</style>
